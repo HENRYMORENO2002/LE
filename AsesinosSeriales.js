d7978efb5075e8c4694ce195b0b809b9e2c9be6b
HENRYMORENO2002 <153622460+HENRYMORENO2002@users.noreply.github.com>
2023-12-12 07:38:46 -0600

    Add files via upload

---
+++ AsesinosSeriales.js
@@ -0,0 +1,41 @@
+window.onload = ejecutaEventos;
+
+function AsesinoDelMuelle() {
+    alert("Asesino del muelle");
+}
+
+function BajoElCuchillo() {
+    alert("Bajo el cuchillo");
+}
+
+function ExperimentosSecretos() {
+    alert("Experimentos secretos");
+}
+
+function FuenteDeCooper() {
+    alert("Fuente de Cooper");
+}
+
+function Invocacion() {
+    alert("Invocación");
+}
+
+function MacabroCarnicero() {
+    alert("Macabro carnicero");
+}
+
+function QuemadoHastaLosHuesos() {
+    alert("Quemado hasta los huesos");
+}
+
+function SangreDeFamilia() {
+    alert("Sangre de familia");
+}
+
+function SangreEnElPisoBirsatil() {
+    alert("Sangre en el piso bursatil");
+}
+
+function UltimaCena() {
+    alert("Última cena");
+}
